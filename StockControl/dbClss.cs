754aaa2554c2ecef3ec6a8cf69066d7f4577d86a
Tay-nakub <tay_nakub@outlook.com>
2017-06-03 16:05:43 +0700

    dbclass

--- StockControl/dbClss.cs
+++ StockControl/dbClss.cs
@@ -19,7 +19,7 @@ namespace StockControl
 {
     class dbClss
     {
-        public string versioin = "ss";
+        public string versioin = "ss1";
         public static Telerik.WinControls.UI.RadRibbonForm CreateForm(string form)
         {
             try
