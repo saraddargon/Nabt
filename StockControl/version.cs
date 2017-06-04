8b63f43a20be76c56f05aac8e7bd006029aa2ac4
Tay-nakub <tay_nakub@outlook.com>
2017-06-04 13:36:19 +0700

    add version class

---
+++ StockControl/version.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockControl
+{
+    class version
+    {
+        //Add Folder Report and Screen Report ##04/06/2017 13:35 PM.
+    }
+}
